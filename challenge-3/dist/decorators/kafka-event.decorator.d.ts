0e79592d96edc45a702aa02a770a94d07bdb113d
fbarrioss <franklin.barrios@icloud.com>
2026-04-03 13:12:08 -0500

    Limpieza de carpetas

--- challenge-3/dist/decorators/kafka-event.decorator.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const KAFKA_EVENT_METADATA: unique symbol;
-export declare function KafkaEvent(topic: string): MethodDecorator;
--- challenge-3/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './kafka.module';
-export * from './kafka-typed-producer';
-export * from './decorators/kafka-event.decorator';
-export * from './types/event-contract.type';
